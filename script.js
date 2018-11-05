233bb61762dcd52cc8d27db9419da5a6c30e0f4f
Carlos Moura <m.informatica@yahoo.com.br>
2018-11-05 00:02:22 -0200

    Criada a função ler_arquivo em script.js para manusear o arquivo de texto que contem os livros

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function principal(){
+	var arquivo;
+	var todosOsLivros;
+	var ordenacao;
+	var livrosOrdenados;
+	var html;
 
+	try {
+
+    	arquivo = ler_Arquivo();
+
+	} catch(e) {
+
+	    alert(e);
+
+	}
+}
+
+function ler_Arquivo(){
+	var arquivo 		= requisicao_Arquivo(g_URL + "livros.txt");
+
+	return arquivo;
 }
