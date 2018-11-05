f86fa545b6d3f1788bfe89c3f18b891ed2a89c5b
Carlos Moura <m.informatica@yahoo.com.br>
2018-11-05 00:04:25 -0200

    Melhorada a função ler_arquivo em script.js, esperando outras funções internas de validação para auxiliar nas regras do módulo

--- script.js
+++ script.js
@@ -19,5 +19,39 @@ function principal(){
 function ler_Arquivo(){
 	var arquivo 		= requisicao_Arquivo(g_URL + "livros.txt");
 
-	return arquivo;
+	var resultado 		= "";
+	var nomesAtributos 	= "";
+	var livros         	= "";
+	var ordenacao      	= "";
+	var objetoLivro    	= {};
+	var livrosTratados 	= [];
+
+	arquivo = verifica_Arquivo(arquivo);
+
+	if(arquivo != ""){
+		nomesAtributos  = arquivo[0];
+		livros 		 	= arquivo[1];
+		ordenacao   	= arquivo[2];
+
+		for(var i = 0; i < livros.length; i++){
+
+			objetoLivro = {};
+			livros[i] = livros[i].split(";");
+
+			for(var j = 0; j < livros[i].length; j++){
+
+				objetoLivro[nomesAtributos[j]] = livros[i][j];
+
+			}
+
+			livrosTratados.push(objetoLivro);
+
+		}
+
+		resultado = [livrosTratados, ordenacao];
+
+	}
+
+	return resultado;
+
 }
