e2e15c0ea63348883d06e0d2defb325ebee802be
Carlos Moura <m.informatica@yahoo.com.br>
2018-11-05 00:05:55 -0200

    Função verifica_arquivo criada para garantir que o arquivo recebido não quebra nenhuma regra esperada

--- script.js
+++ script.js
@@ -55,3 +55,59 @@ function ler_Arquivo(){
 	return resultado;
 
 }
+
+function verifica_Arquivo(arquivo){
+	var nomesAtributos 	  		= "";
+	var livros         	  		= "";
+	var ordenacao      	  		= "";
+	var qtdAtributosLivro 		= "";
+	var qtdAtributosAnunciados  = "";
+
+	if(arquivo.includes("***") && arquivo.includes("$$$")){
+		arquivo    		= arquivo.replace(/\r?\n|\r/g, "");
+		arquivo    		= arquivo.split("***");
+		arquivo[1] 		= arquivo[1].split("$$$");
+
+		nomesAtributos 	= arquivo[0].split(";");
+		livros 			= arquivo[1][0].split("|?|");
+		ordenacao 		= arquivo[1][1].split("|?|");
+
+		if(nomesAtributos[nomesAtributos.length - 1] == ""){ nomesAtributos.pop(); }
+		if(ordenacao[ordenacao.length - 1] == ""){ ordenacao.pop(); }
+		if(livros[livros.length - 1] == ""){ livros.pop(); }
+
+		qtdAtributosLivro      = livros[0].split(";").length;
+		qtdAtributosAnunciados = nomesAtributos.length;
+
+		if(qtdAtributosAnunciados == qtdAtributosLivro){
+			if(ordenacao != ""){
+				var resultado = verifica_Ordenacao(ordenacao, nomesAtributos);
+				if(resultado === true){
+
+					arquivo = [nomesAtributos, livros, ordenacao];
+
+				} else {
+
+					throw new OrderingException(resultado);
+
+				}
+			} else {
+
+				arquivo = "";
+
+			}
+
+		} else {
+
+			throw new OrderingException("Quantidade de atributos anunciada não é igual a quantidade utilizada.");
+
+		}
+
+		return arquivo;
+
+	} else {
+
+		throw new OrderingException("Arquivo de texto não está configurado corretamente.\nVerificar *** e $$$");
+
+	}
+}
