59bce8ee2a447b6d0d38cb6e6b942f994db41417
Carlos Moura <m.informatica@yahoo.com.br>
2019-08-27 22:00:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,344 +1,305 @@
 OrderingException.prototype = Object.create(Error.prototype);
-var g_URL = "/Modulo-gestao_biblioteca/";
 
-function principal(){
-	var arquivo;
-	var nomesAtributos;
-	var todosOsLivros;
-	var ordenacao;
-	var livrosOrdenados;
-	var html;
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('files').addEventListener('change', loadFile, false);
+});
 
-	try {
+function loadFile(e) {
+  const input = e.target;
+  try {
+    var reader = new FileReader();
+    reader.onload = function(){
+      run(reader.result);
+    };
 
-    	arquivo = ler_Arquivo();
-
-	} catch(e) {
-
-	    alert(e);
-
-	}
-
-	if(arquivo != "" && arquivo != undefined){
-		nomesAtributos = arquivo[0];
-		todosOsLivros  = arquivo[1];
-		ordenacao      = arquivo[2];
-
-		livrosOrdenados = servico_Ordenacao(todosOsLivros, ordenacao);
-
-		html = construir_HTML(nomesAtributos, livrosOrdenados);
-		mostra_HTML("vitrineLivros", html);
-	}
-
+    reader.readAsText(input.files[0]);
+  } catch(e) {
+    alert(e);
+  }
 }
 
-function ler_Arquivo(){
-	var arquivo 		= requisicao_Arquivo(g_URL + "livros.txt");
+function run(fileText){
+  fileText = validateFile(fileText);
+  fileText = parseFile(fileText)
+  
 
-	var resultado 		= "";
-	var nomesAtributos 	= "";
-	var livros         	= "";
-	var ordenacao      	= "";
-	var objetoLivro    	= {};
-	var livrosTratados 	= [];
-
-	arquivo = verifica_Arquivo(arquivo);
-
-	if(arquivo != ""){
-		nomesAtributos  = arquivo[0];
-		livros 		 	= arquivo[1];
-		ordenacao   	= arquivo[2];
-
-		for(var i = 0; i < livros.length; i++){
-
-			objetoLivro = {};
-			livros[i] = livros[i].split(";");
-
-			for(var j = 0; j < livros[i].length; j++){
-
-				objetoLivro[nomesAtributos[j]] = livros[i][j];
-
-			}
-
-			livrosTratados.push(objetoLivro);
-
-		}
-
-		resultado = [nomesAtributos, livrosTratados, ordenacao];
-
-	}
-
-	return resultado;
-
+  show(fileText)
 }
 
-function verifica_Arquivo(arquivo){
-	var nomesAtributos 	  		= "";
-	var livros         	  		= "";
-	var ordenacao      	  		= "";
-	var qtdAtributosLivro 		= "";
-	var qtdAtributosAnunciados  = "";
+function show(fileText) {
+  let nomesAtributos;
+  let todosOsLivros;
+  let ordenacao;
+  let livrosOrdenados;
+  let html;
+ 
 
-	if(arquivo.includes("***") && arquivo.includes("$$$")){
-		arquivo    		= arquivo.replace(/\r?\n|\r/g, "");
-		arquivo    		= arquivo.split("***");
-		arquivo[1] 		= arquivo[1].split("$$$");
+  if(fileText != "" && fileText != undefined){
+    nomesAtributos = fileText[0];
+    todosOsLivros  = fileText[1];
+    ordenacao      = fileText[2];
 
-		nomesAtributos 	= arquivo[0].split(";");
-		livros 			= arquivo[1][0].split("|?|");
-		ordenacao 		= arquivo[1][1].split("|?|");
+    livrosOrdenados = servico_Ordenacao(todosOsLivros, ordenacao);
 
-		if(nomesAtributos[nomesAtributos.length - 1] == ""){ nomesAtributos.pop(); }
-		if(ordenacao[ordenacao.length - 1] == ""){ ordenacao.pop(); }
-		if(livros[livros.length - 1] == ""){ livros.pop(); }
+    html = construir_HTML(nomesAtributos, livrosOrdenados);
+    mostra_HTML("vitrineLivros", html);
+  }
+}
 
-		qtdAtributosLivro      = livros[0].split(";").length;
-		qtdAtributosAnunciados = nomesAtributos.length;
+function parseFile(fileText){
+    var resultado 		= "";
+    var nomesAtributos 	= "";
+    var livros         	= "";
+    var ordenacao      	= "";
+    var objetoLivro    	= {};
+    var livrosTratados 	= [];
 
-		if(qtdAtributosAnunciados == qtdAtributosLivro){
-			if(ordenacao != ""){
-				var resultado = verifica_Ordenacao(ordenacao, nomesAtributos);
-				if(resultado === true){
+    if(fileText != ""){
+      nomesAtributos  = fileText[0];
+      livros 		 	= fileText[1];
+      ordenacao   	= fileText[2];
 
-					arquivo = [nomesAtributos, livros, ordenacao];
+      for(var i = 0; i < livros.length; i++){
+        objetoLivro = {};
+        livros[i] = livros[i].split(";");
 
-				} else {
+        for(var j = 0; j < livros[i].length; j++){
+          objetoLivro[nomesAtributos[j]] = livros[i][j];
+        }
+        livrosTratados.push(objetoLivro);
+      }
+      resultado = [nomesAtributos, livrosTratados, ordenacao];
+    }
 
-					throw new OrderingException(resultado);
+    return resultado;
+}
 
-				}
-			} else {
+function validateFile(fileText){
+    var nomesAtributos 	  		= "";
+    var livros         	  		= "";
+    var ordenacao      	  		= "";
+    var qtdAtributosLivro 		= "";
+    var qtdAtributosAnunciados  = "";
 
-				arquivo = "";
+    if(fileText.includes("***") && fileText.includes("$$$")){
+      fileText    		= fileText.replace(/\r?\n|\r/g, "");
+      fileText    		= fileText.split("***");
+      fileText[1] 		= fileText[1].split("$$$");
 
-			}
+        nomesAtributos 	= fileText[0].split(";");
+        livros 			= fileText[1][0].split("|?|");
+        ordenacao 		= fileText[1][1].split("|?|");
 
-		} else {
+        if(nomesAtributos[nomesAtributos.length - 1] == ""){ nomesAtributos.pop(); }
+        if(ordenacao[ordenacao.length - 1] == ""){ ordenacao.pop(); }
+        if(livros[livros.length - 1] == ""){ livros.pop(); }
 
-			throw new OrderingException("Quantidade de atributos anunciada não é igual a quantidade utilizada.");
+        qtdAtributosLivro      = livros[0].split(";").length;
+        qtdAtributosAnunciados = nomesAtributos.length;
 
-		}
+        if(qtdAtributosAnunciados == qtdAtributosLivro){
+            if(ordenacao != ""){
+              var resultado = verifica_Ordenacao(ordenacao, nomesAtributos);
+              if(resultado === true){
+                fileText = [nomesAtributos, livros, ordenacao];
+              } else {
+                throw new OrderingException(resultado);
+              }
+            } else {
+              fileText = "";
+            }
+        } else {
+          throw new OrderingException("Quantidade de atributos anunciada não é igual a quantidade utilizada.");
+        }
 
-		return arquivo;
-
-	} else {
-
-		throw new OrderingException("Arquivo de texto não está configurado corretamente.\nVerificar *** e $$$");
-
-	}
+        return fileText;
+    } else {
+      throw new OrderingException("Arquivo de texto não está configurado corretamente.\nVerificar *** e $$$");
+    }
 }
 
 function verifica_Ordenacao(ordenacao, nomesAtributos){
-	var resultado = true;
-	var encontrou = false;
-	var atributoOrdenado;
-	var direcaoOrdenacao;
+    var resultado = true;
+    var encontrou = false;
+    var atributoOrdenado;
+    var direcaoOrdenacao;
 
-	for(var i = 0; i < ordenacao.length; i++){
+    for(var i = 0; i < ordenacao.length; i++){
 
-		ordenacao[i] = ordenacao[i].split(";");
+        ordenacao[i] = ordenacao[i].split(";");
 
-		if(ordenacao[i].length == 2){
+        if(ordenacao[i].length == 2){
 
-			atributoOrdenado = ordenacao[i][0];
-			direcaoOrdenacao = ordenacao[i][1];
+            atributoOrdenado = ordenacao[i][0];
+            direcaoOrdenacao = ordenacao[i][1];
 
-			if(direcaoOrdenacao == "Ascendente" || direcaoOrdenacao == "Descendente"){
+            if(direcaoOrdenacao == "Ascendente" || direcaoOrdenacao == "Descendente"){
 
-				for(var j = 0; j < nomesAtributos.length; j++){
+                for(var j = 0; j < nomesAtributos.length; j++){
 
-					if(atributoOrdenado == nomesAtributos[j]){
+                    if(atributoOrdenado == nomesAtributos[j]){
 
-						j = nomesAtributos.length;
-						encontrou = true;
+                        j = nomesAtributos.length;
+                        encontrou = true;
 
-					} else if( j == nomesAtributos.length - 1 && encontrou == false ) {
+                    } else if( j == nomesAtributos.length - 1 && encontrou == false ) {
 
-						resultado = "O arquivo contém erros de configuração de ordenação!\nErro encontrando na palavra: " + ordenacao[i];
-						i = ordenacao.length;
-						
-					}
+                        resultado = "O arquivo contém erros de configuração de ordenação!\nErro encontrando na palavra: " + ordenacao[i];
+                        i = ordenacao.length;
+                        
+                    }
 
-				}
+                }
 
-			} else {
+            } else {
 
-				resultado = "O arquivo contém erros de configuração de ordenação!\nErro encontrando na direção utilizada: " +direcaoOrdenacao;
-				i = ordenacao.length;
+                resultado = "O arquivo contém erros de configuração de ordenação!\nErro encontrando na direção utilizada: " +direcaoOrdenacao;
+                i = ordenacao.length;
 
-			}
+            }
 
-		} else {
+        } else {
 
-			resultado = "O arquivo contém erros de configuração de ordenação!\nA configuração deve ser realizada da seguinte forma:\nAtributo_a_ser_ordenado;Direção_ordenação";
-			i = ordenacao.length;
+            resultado = "O arquivo contém erros de configuração de ordenação!\nA configuração deve ser realizada da seguinte forma:\nAtributo_a_ser_ordenado;Direção_ordenação";
+            i = ordenacao.length;
 
-		}
-		
-		encontrou = false;
+        }
+        
+        encontrou = false;
 
-	}
+    }
 
-	return resultado;
-}
-
-function requisicao_Arquivo(caminho){
-    var arquivo = new XMLHttpRequest();
-    var resultado;
-
-    arquivo.open("GET", caminho, false);
-    arquivo.setRequestHeader("Cache-Control", "no-cache");
-    arquivo.onload = function (e) {
-
-		if (arquivo.readyState == XMLHttpRequest.DONE) {
-
-		  	if (arquivo.status == 200) {
-
-		  		resultado = arquivo.responseText;
-
-			}else{
-
-				alert("Erro: \n"+arquivo.statusText);
-				resultado = false;
-
-			}
-
-		}
-	};
-
-	arquivo.send();
-	return resultado;
+    return resultado;
 }
 
 function servico_Ordenacao(livros, ordenacao){
-	var atributo;
-	var direcao;
-	var atributoAnterior;
+    var atributo;
+    var direcao;
+    var atributoAnterior;
 
-	for(var i = 0; i < ordenacao.length; i++){
+    for(var i = 0; i < ordenacao.length; i++){
 
-		atributo 		 = ordenacao[i][0];
-		direcao  		 = ordenacao[i][1];
-		livros   		 = realiza_ordenacao(livros, atributo, atributoAnterior, direcao);
-		atributoAnterior = atributo;
+        atributo 		 = ordenacao[i][0];
+        direcao  		 = ordenacao[i][1];
+        livros   		 = realiza_ordenacao(livros, atributo, atributoAnterior, direcao);
+        atributoAnterior = atributo;
 
-	}
+    }
 
-	return livros;
+    return livros;
 }
 
 function realiza_ordenacao(livros, atributo, atributoAnterior, direcao){
 
-	livros.sort(function(a, b) {
+    livros.sort(function(a, b) {
 
-		if(atributoAnterior == ""){
+        if(atributoAnterior == ""){
 
-			return algoritmo_ordenacao(a, b, atributo, direcao);
+            return algoritmo_ordenacao(a, b, atributo, direcao);
 
-		} else if(a[atributoAnterior] == b[atributoAnterior]) {
+        } else if(a[atributoAnterior] == b[atributoAnterior]) {
 
-			return algoritmo_ordenacao(a, b, atributo, direcao);
+            return algoritmo_ordenacao(a, b, atributo, direcao);
 
-		}
+        }
 
-	});
+    });
 
-	return livros;
+    return livros;
 }
 
 function algoritmo_ordenacao(a, b, atributo, direcao){
-	if(isNaN(a[atributo] - b[atributo])){
+    if(isNaN(a[atributo] - b[atributo])){
 
-		if (a[atributo] < b[atributo]) {
-			if(direcao == "Ascendente"){
+        if (a[atributo] < b[atributo]) {
+            if(direcao == "Ascendente"){
 
-				return -1;
+                return -1;
 
-			}else{
+            }else{
 
-				return 1;
+                return 1;
 
-			}
-		}
+            }
+        }
 
-		if (a[atributo] > b[atributo]) {
+        if (a[atributo] > b[atributo]) {
 
-			if(direcao == "Ascendente"){
+            if(direcao == "Ascendente"){
 
-				return 1;
+                return 1;
 
-			}else{
+            }else{
 
-				return -1;
-				
-			}
+                return -1;
+                
+            }
 
-		}
+        }
 
-		return 0;
+        return 0;
 
-	}else{
+    }else{
 
-		if(direcao == "Ascendente"){
+        if(direcao == "Ascendente"){
 
-			return a[atributo] - b[atributo];
+            return a[atributo] - b[atributo];
 
-		}else{
+        }else{
 
-			return b[atributo] - a[atributo];
-			
-		}
+            return b[atributo] - a[atributo];
+            
+        }
 
-	}
+    }
 }
 
 function construir_HTML(nomesAtributos, livros){
-	var html = "";
+    var html = "";
 
-	html += "<table border='1px' width='600px'>";
-	html += "<tr>";
+    html += "<table border='1px' width='600px'>";
+    html += "<tr>";
 
-	for(var i = 0; i < nomesAtributos.length; i++){
+    for(var i = 0; i < nomesAtributos.length; i++){
 
-		html += "<th>";
-		html += nomesAtributos[i];
-		html += "</th>";
+        html += "<th>";
+        html += nomesAtributos[i];
+        html += "</th>";
 
-	}
+    }
 
-	html += "</tr>";
+    html += "</tr>";
 
-	for(var j = 0; j < livros.length; j++){
+    for(var j = 0; j < livros.length; j++){
 
-		html += "<tr>";
+        html += "<tr>";
 
-		for(var k = 0; k < nomesAtributos.length; k++){
+        for(var k = 0; k < nomesAtributos.length; k++){
+            html += "<td>";
+            html += livros[j][nomesAtributos[k]];
+            html += "</td>";
+        }
+        
+        html += "</tr>";
 
-			html += "<td>";
-			html += livros[j][nomesAtributos[k]];
-			html += "</td>";
-		}
-		
-		html += "</tr>";
+    }
 
-	}
+    html += "</table>";
 
-	html += "</table>";
-
-	return html;
+    return html;
 }
 
 function mostra_HTML(local, html){
 
-	document.getElementById(local).innerHTML = html;
+    document.getElementById(local).innerHTML = html;
 
 }
 
 function OrderingException(message) {
 
-	const error = new Error(message);
-	console.error(message);
+    const error = new Error(message);
+    console.error(message);
 
-	return error;
+    return error;
 
 }
